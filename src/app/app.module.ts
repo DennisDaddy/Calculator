ee27ac2265cdc27c447a8c297a247b2b5e4d6e0f
DennisDaddy <softteez95@gmail.com>
2017-10-22 18:12:22 +0300

    remove some scaffolds

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,7 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -14,9 +13,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    AboutPage,    
     TabsPage
   ],
   imports: [
@@ -26,9 +23,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    AboutPage,    
     TabsPage
   ],
   providers: [
